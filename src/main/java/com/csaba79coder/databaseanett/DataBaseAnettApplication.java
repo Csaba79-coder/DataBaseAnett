62c318542a850a5df05e9a4495881586d80c5810
Csaba79-coder <csabavadasz79@gmail.com>
2022-05-26 15:41:46 +0200

    Initial commit

---
+++ src/main/java/com/csaba79coder/databaseanett/DataBaseAnettApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.databaseanett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseAnettApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBaseAnettApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/databaseanett/DataBaseAnettApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.databaseanett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBaseAnettApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
